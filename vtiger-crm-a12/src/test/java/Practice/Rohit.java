a7d6c152731c669114b402b572e4174671318087
Rohit Sharma <Rohit Sharma@Laptop-Rohit>
2026-01-23 00:00:20 +0530

    jnn

--- vtiger-crm-a12/src/test/java/Practice/Rohit.java
+++
@@ -1,9 +0,0 @@
-package Practice;
-
-class Rohit
-{
-public static void main (String [] args)
-{
-System.out.println("22-1-26");
-}
-}
--- vtiger-crm-a12/src/test/java/Practice/Rohit3.java
+++
@@ -1,9 +0,0 @@
-package Practice;
-
-class Rohit2
-{
-public static void main (String [] args)
-{
-System.out.println("23:55, 22--1-26");
-}
-}
